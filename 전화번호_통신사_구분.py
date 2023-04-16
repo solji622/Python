5492501704ad6b93d6c41ed0016fef93ba24200a
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2023-04-16 10:44:21 +0900

    Delete 전화번호_통신사_구분.py

--- 전화번호_통신사_구분.py
+++
@@ -1,9 +0,0 @@
-num = input("휴대전화 번호 입력: ")
-if "011-" in num : #num 안에 011- 이 있다면
-  print("당신은 SKT 사용자 입니다")
-elif "016-" in num : #num 안에 016- 이 있다면
-  print("당신은 KT 사용자 입니다")
-elif "019-" in num : #num 안에 019- 이 있다면
-  print("당신은 LGU 사용자 입니다")
-if "010-" in num : #num 안에 010- 이 있다면
-  print("당신은 알수없음 입니다")
